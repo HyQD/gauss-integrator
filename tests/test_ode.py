1899ffdc6afb9727a95cf676e8f3692b5d16b407
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-02-28 13:16:57 +0100

    Add working test

--- tests/test_ode.py
+++ tests/test_ode.py
@@ -9,14 +9,15 @@ def rhs(t, y, arg1=1):
 
 def test_1d_ode():
     r = ode(rhs).set_integrator("GaussIntegrator")
-    r = ode(rhs).set_integrator("zvode")
-    # r = complex_ode(rhs).set_integrator("GaussIntegrator")
+    r_2 = ode(rhs).set_integrator("zvode")
     r.set_initial_value([1], 0)
+    r_2.set_initial_value([1], 0)
 
     t1 = 10
     dt = 1e-1
+    eps = 1e-4
 
-    while r.successful() and r.t < t1:
-        print(r.t + dt, r.integrate(r.t + dt))
-
-    assert False
+    while r.successful() and r_2.successful() and r.t < t1 and r_2.t < t1:
+        g_int = r.integrate(r.t + dt)
+        z_int = r_2.integrate(r_2.t + dt)
+        assert abs(g_int - z_int) < eps
