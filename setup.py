4a37a2cff19fde0c37aa6f65868639e8d094b015
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-03-19 09:28:14 +0100

    Add required packages to setup
        Also, rename package in all lowercase letters.

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="Gauss-integrator", version="0.0.1", packages=find_packages())
+setup(
+    name="gauss-integrator",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=["numpy", "scipy",],
+)
